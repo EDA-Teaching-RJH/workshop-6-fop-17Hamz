6372f74a3963eca2888ce22dc8c14195b18e6be1
17Hamz <iah6@kent.ac.uk>
2026-02-11 11:54:54 +0000

    Storage bay Analysing samples new findings

--- part_1.py
+++ part_1.py
@@ -1,1 +1,19 @@
+# The storage bay 
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
+print(sample_bay[0])
+print(sample_bay[-1])
+print(len(sample_bay))
+
+
+#Analysing Samples (Iteration)
+for sample in sample_bay:
+    print(f"Transmitting data for: {sample}")
+
+
+new_findings = []
+for _ in range(3):
+    rock_type = input("Enter a new rock type: ")
+    new_findings.append(rock_type)
+    print(new_findings)
+
 
